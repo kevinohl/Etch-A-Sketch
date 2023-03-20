322a789c1a66b6a2ad3f619bda6ac3760bac41e7
Kevin Ohl <kevinohl@msn.com>
2023-03-20 16:11:27 +0100

    Fix style errors using linter

--- js/js-file.js
+++ js/js-file.js
@@ -1,45 +1,44 @@
-//alert("hewwo");
+// alert("hewwo");
 
-const container = document.querySelector("#grid-container");
-const layoutButton = document.querySelector("#layout-btn");
+const container = document.querySelector('#grid-container');
+const layoutButton = document.querySelector('#layout-btn');
 const gridWidth = 960;
 const startingSquaresPerRow = 20;
 
 function setLayout(squaresPerRow) {
-    // erase existing grid
-    container.innerHTML = "";
-    if (squaresPerRow > 100) squaresPerRow = 100;
+  // erase existing grid
+  container.innerHTML = '';
+  if (squaresPerRow > 100) squaresPerRow = 100;
+  for (let i = 0; i < squaresPerRow; i++) {
+    const rowContainer = document.createElement('div');
+    rowContainer.classList.add('row-container');
     for (let i = 0; i < squaresPerRow; i++) {
-        const rowContainer = document.createElement("div");
-        rowContainer.classList.add("row-container");
-        for (let i = 0; i < squaresPerRow; i++) {
-            const gridSquare = document.createElement("div");
-            gridSquare.classList.add("grid-square");
-            gridSquare.style.width = `${gridWidth / squaresPerRow}px`;
-            gridSquare.style.height = `${gridWidth / squaresPerRow}px`;
-            gridSquare.style["background-color"] = "rgb(255,255,255)";
-            gridSquare.addEventListener("mouseover", colorSquare);
-            rowContainer.appendChild(gridSquare);
-        }
-        container.appendChild(rowContainer);
+      const gridSquare = document.createElement('div');
+      gridSquare.classList.add('grid-square');
+      gridSquare.style.width = `${gridWidth / squaresPerRow}px`;
+      gridSquare.style.height = `${gridWidth / squaresPerRow}px`;
+      gridSquare.style['background-color'] = 'rgb(255,255,255)';
+      gridSquare.addEventListener('mouseover', colorSquare);
+      rowContainer.appendChild(gridSquare);
     }
+    container.appendChild(rowContainer);
+  }
 }
 
 function colorSquare() {
-    currentColor = this.style["background-color"];
-    rgbValues = currentColor.substring(4, currentColor.length-1).split(",");
-    newRgbValues = [];
-    for (colorValue of rgbValues) {
-        if (colorValue - 25 < 0) newRgbValues.push(0);
-        else newRgbValues.push(colorValue - 25);
-    }
-    newColor = `rgb(${newRgbValues.join()})`;
-    this.style["background-color"] = newColor;
+  const currentColor = this.style['background-color'];
+  const rgbValues = currentColor.substring(4, currentColor.length - 1).split(',');
+  const newRgbValues = [];
+  for (const colorValue of rgbValues) {
+    if (colorValue - 25 < 0) newRgbValues.push(0);
+    else newRgbValues.push(colorValue - 25);
+  }
+  const newColor = `rgb(${newRgbValues.join()})`;
+  this.style['background-color'] = newColor;
 }
 
-layoutButton.addEventListener("click", function () {
-    setLayout(prompt("How many squares per row?"));
+layoutButton.addEventListener('click', () => {
+  setLayout(prompt('How many squares per row?'));
 });
-
 
 setLayout(startingSquaresPerRow);
